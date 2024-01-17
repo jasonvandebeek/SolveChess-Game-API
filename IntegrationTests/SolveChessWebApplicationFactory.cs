393474a8f13f841c362170ec9cc074e1fab2f349
jasonvandebeek <jasonvandebeek@gmail.com>
2024-01-17 15:20:09 +0100

    test: fix integration test setup

--- IntegrationTests/SolveChessWebApplicationFactory.cs
+++ IntegrationTests/SolveChessWebApplicationFactory.cs
@@ -28,6 +28,7 @@ internal class SolveChessWebApplicationFactory : WebApplicationFactory<Program>
                 });
             });
 
+            Environment.SetEnvironmentVariable("SolveChess_CorsUrls", "https://localhost:3000");
             Environment.SetEnvironmentVariable("SolveChess_JwtSecret", "TestSecretKeyForJwtTokensInIntegrationTests");
         });
     }
